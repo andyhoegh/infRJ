016a78ad38e95ee60081b0c47511ac7f1b08dc4b
andyhoegh <andrew.hoegh@montana.edu>
2024-04-16 10:34:49 -0600

    extract landuse from USGS

---
+++ R/extract_landuse.R
@@ -0,0 +1,17 @@
+extract_landuse <- function( lat, long, SiteName, landuse2021, buffer = 100  ){
+  point <- ee$Geometry$Point(long, lat)
+  point_buffer <- ee$Geometry$buffer(point, buffer) # in meters
+
+  landuse <- landuse2021$reduceRegion(
+    reducer = ee$Reducer$frequencyHistogram(),
+    geometry = point_buffer,
+    scale = 30
+  )
+
+  tmp <- landuse$getInfo()$landcover
+
+  tibble(cat = names(tmp),pixels = unlist(tmp)) %>%
+    dplyr::mutate(prop = pixels / sum(pixels),
+           SiteName = SiteName,
+           range = buffer)
+}
