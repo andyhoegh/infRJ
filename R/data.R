7e7d382fe0b50f30e96cb104efea3fd3bc3adb5e
andyhoegh <andrew.hoegh@montana.edu>
2024-04-16 11:56:56 -0600

    added source dataset

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Coordinates
+#'
+#' Coordinates for potential bat roosts
+#'
+#' @format ## `coords`
+#' A data frame with 1000 rows and 2 columns:
+#' \describe{
+#'   \item{x}{longitude}
+#'   \item{y}{ latitude}
+#' }
+#' @source Obtained from National Bridge Inventory database
